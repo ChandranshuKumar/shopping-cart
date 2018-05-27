2dce1403fc50cd0ea6597eba90193632f9b26d75
Kumar Chandranshu <chandranshu040@gmail.com>
2018-05-27 15:07:27 +0000

    added products

--- product-seed.js
+++ product-seed.js
@@ -3,28 +3,46 @@ var Product = require("./models/product");
 
 var products = [
     {
-        image: "https://upload.wikimedia.org/wikipedia/en/thumb/2/28/Assassin%27s_Creed_IV_-_Black_Flag_cover.jpg/220px-Assassin%27s_Creed_IV_-_Black_Flag_cover.jpg",
-        title: "Black Flag",
+        image: "https://images-eu.ssl-images-amazon.com/images/I/61FN63zv-cL._SL150_.jpg",
+        title: "JBL GO",
         offer: "20% OFF and more",
-        price: 340
+        price: 1700
     },
     {
-        image: "http://www.kmart.com.au/wcsstore/Kmart/images/ncatalog/tf/3/67758203-1-tf.jpg",
-        title: "Roadster",
+        image: "https://images-eu.ssl-images-amazon.com/images/I/41ZjKnb4a3L._AC_US160_.jpg",
+        title: "SanDisk Cruzer Blade 16GB",
         offer: "10% OFF and more",
-        price: 540
+        price: 500
     },
     {
-        image: "https://images-na.ssl-images-amazon.com/images/I/71KFFvyjGbL._SL500_SR200,200_.jpg",
-        title: "Apple Cider Vineger",
+        image: "https://images-eu.ssl-images-amazon.com/images/I/41afx-tDYqL._AC_US160_.jpg",
+        title: "TLC 562 VR",
         offer: "50% OFF and more",
-        price: 50
+        price: 9999
     },
     {
-        image: "https://d1s2zprapij148.cloudfront.net/image/cache/catalog/products/15680-200x200.jpg",
-        title: "Bucket",
+        image: "https://images-eu.ssl-images-amazon.com/images/I/31BHWx4ql7L._AC_US218_.jpg",
+        title: "Sony PS4 1 TB Slim Console",
         offer: "40% OFF and more",
-        price: 120
+        price: 32000
+    },
+    {
+        image: "https://images-eu.ssl-images-amazon.com/images/I/31LPmQMJm0L._AC_US160_.jpg",
+        title: "Sennheiser CX 180 Earphone",
+        offer: "40% OFF and more",
+        price: 800
+    },
+    {
+        image: "https://images-eu.ssl-images-amazon.com/images/I/51+96dogGHL._AC_US160_.jpg",
+        title: "Nike Strike",
+        offer: "40% OFF and more",
+        price: 520
+    },
+    {
+        image: "https://images-eu.ssl-images-amazon.com/images/I/81QUSIDc2NL._SL150_.jpg",
+        title: "Cosmic Byte CB-M-05 Pulsar V2",
+        offer: "40% OFF and more",
+        price: 625
     }
 ];
 
